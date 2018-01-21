037198b57294a0a5266e5b644218bd9b8937020f
Kenneth Jao <ksjdragon@gmail.com>
2018-01-20 21:35:29 -0500

    added jquery, animations, favicon

--- static/index.js
+++ static/index.js
@@ -1,13 +1,22 @@
-var navi = [ // Array containing navigation items in form [Font-Awesome class name, Display Text].
-	["bar-chart", "Data Values"],
-	["database", "Database and Files"],
-	["bell", "Updates and Progress"]
+var navSelect = "home";
+
+var navi = [ // Array containing navigation items in form [Font-Awesome class name, Display Text, Onclick function].
+	["home", "Home", "home"],
+	["bar-chart", "Data Values", "dataValues"],
+	["database", "Database and Files", "files"],
+	["bell", "Updates and Progress", "updates"]
 ];
 
 for(var i = 0; i < navi.length; i++) { // Create navigation tabs.
 	var side = document.getElementById("sidebar");
 	var div = document.createElement("div");
-	div.className = "navi colorFade";
+	div.className = "navi transition";
+	div.setAttribute("option", navi[i][2]);
+	div.onclick = function() {
+		var op = this.getAttribute("option");
+		if(navSelect === op) return;
+		updateMain(op);
+	};
 	var ic = document.createElement("i");
 	ic.className = "fa fa-"+navi[i][0];
 	ic["aria-hidden"] = true;
@@ -18,3 +27,26 @@ for(var i = 0; i < navi.length; i++) { // Create navigation tabs.
 	side.appendChild(div);
 }
 
+updateNav(navSelect);
+
+function updateMain(op) {
+	updateNav(op);
+	document.getElementById(navSelect).style.opacity = "0";
+	setTimeout(function() {
+		document.getElementById(navSelect).style.display = "none";
+		document.getElementById(op).style.display = "grid";
+		setTimeout(function() {
+			document.getElementById(op).style.opacity = "1";
+		}, 30);
+		navSelect = op;
+	}, 300);
+}
+
+function updateNav(op) {
+	var oldNav = document.querySelectorAll("[option="+navSelect+"]")[0];
+	var newNav = document.querySelectorAll("[option="+op+"]")[0];
+	oldNav.style.backgroundColor = "rgba(0,0,0,0)";
+	oldNav.style.color = "white";
+	newNav.style.backgroundColor = "#F8F3F0";
+	newNav.style.color = "#F47922";
+}
